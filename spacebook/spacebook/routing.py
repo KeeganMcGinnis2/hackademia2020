fd49ff12cb359a7501af6248e91412c201f37b0c
KeeganMcGinnis2 <keeganmcginnis219@gmail.com>
2020-07-11 01:19:30 -0700

    push for ryon

--- spacebook/spacebook/routing.py
+++ spacebook/spacebook/routing.py
@@ -1,5 +1,11 @@
-from channels.routing import ProtocolTypeRouter
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+import messaging.routing
 
 application = ProtocolTypeRouter({
-    
+    'websocket': AuthMiddlewareStack(
+        URLRouter(
+            messaging.routing.websocket_urlpatterns
+        )
+    ),
 })
--- spacebook/spacebook/settings.py
+++ spacebook/spacebook/settings.py
@@ -123,3 +123,11 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 ASGI_APPLICATION = 'spacebook.routing.application'
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            'hosts': [('127.0.0.1', 6379)],
+        },
+    },
+}
